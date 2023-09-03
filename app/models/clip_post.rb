f285e6813b2c3f450b49e20b49f93ab0771b9584
H.T <hayashi19980203@gmail.com>
2023-09-03 11:16:04 +0900

    tag機能追加

--- app/models/clip_post.rb
+++ app/models/clip_post.rb
@@ -1,2 +1,10 @@
 class ClipPost < ApplicationRecord
+  belongs_to :user
+  has_many :likes
+
+  acts_as_taggable
+  
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  has_many :clip_posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :like_clip_posts, through: :likes, source: :clip_posts
 
   validates :password, confirmation: true
 end
