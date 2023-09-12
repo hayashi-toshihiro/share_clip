225199ea277a786f4d3412ccdde0a676e20b0462
H.T <hayashi19980203@gmail.com>
2023-09-12 09:52:58 +0900

    一部レイアウト調整、利用規約ページ作成

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
+$(document).on('turbolinks:load', function() {
   $("#preview_button, #clip_preview_button").click(function() {
     var url = $("#clip_url").val();
     var content_title = $("#content_title").val();
     var tag_list = $("#tag_list").val();
-    console.log(url);
+
     $.ajax({
         url: "/clip_posts/get_clip",
         method: "GET",
         data: {url: url, content_title: content_title, tag_list: tag_list }
     })
   })
-});
+})
