312041793c9b906ba8577dd64337b2147537d079
H.T <hayashi19980203@gmail.com>
2023-09-20 20:33:15 +0900

    Metaタグ設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def default_meta_tags
+  {
+    site: 'twitchクリップ共有',
+    title: 'ツイッチの動画をシェア検索して視聴者の反応も楽しもう！',
+    reverse: true,
+    separator: '|',   #Webサイト名とページタイトルを区切るために使用されるテキスト
+    description: 'お気に入りクリップを共有しましょう！ゲーム検索・イベント名検索で埋もれた爆笑クリップも発見できるかも！？',
+    keywords: 'ページキーワード',   #キーワードを「,」区切りで設定する
+    noindex: ! Rails.env.production?,
+    icon: [                    #favicon、apple用アイコンを指定する
+      { href: image_url('hasami.jpeg') },
+      { href: image_url('hasami.jpeg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+    ],
+    og: {
+      site_name: 'twitchクリップ共有',
+      title: 'ツイッチの動画をシェア検索して視聴者の反応も楽しもう！',
+      description: 'お気に入りクリップを共有しましょう！ゲーム検索・イベント名検索で埋もれた爆笑クリップも発見できるかも！？', 
+      type: 'website',
+      image: image_url('hasami.jpeg'),
+      locale: 'ja_JP',
+    }
+  }
+  end
 end
+  
