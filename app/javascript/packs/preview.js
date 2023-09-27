6c3a95ada1f97373d850ed8ee4726396cac14950
H.T <hayashi19980203@gmail.com>
2023-09-27 09:14:42 +0900

    バリデーションの修正

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,8 +1,20 @@
 $(document).on('turbolinks:load', function() {
   $("#preview_button, #clip_preview_button").click(function() {
+
+    var url = $("#clip_url").val();
+
+    var urlRegex = /\A.*clip.*twitch\.tv.*\z/;
+
+    if (!url.match(urlRegex)) {
+      var errorMessage = "クリップが見つからないヨ";
+      var flashMessage = $("<div class='alert alert-danger font-6'>" + errorMessage + "</div>");
+      $("#flash_messages").html(flashMessage); 
+      return false;
+    }
+
     var clip_post = {
       clip_post: {
-        url: $("#clip_url").val(),
+        url: url,
         content_title: $("#content_title").val(),
         tag_list: $("#tag_list").val(),
         streamer_list: $("#streamer_list").val(),
