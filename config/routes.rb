99e6a1cee4e712dd906177bf88e677db88252bd8
H.T <hayashi19980203@gmail.com>
2023-10-02 17:36:40 +0900

    レスポンシブ対応

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'test', to: 'test#new'
   get 'clip_posts/get_clip', to: 'clip_posts#get_clip'
   get 'get_user_stamp', to: 'users#get_user_stamp'
+  get 'tags', to: 'tags#index'
 
   resources :users, only: %i[new create]
   resources :clip_posts do
