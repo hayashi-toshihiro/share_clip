95d45e6779fec5e91db1d7f36579c39403712ea3
H.T <hayashi19980203@gmail.com>
2023-10-05 16:51:40 +0900

    heroku_sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,6 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://www.clipreactor.com"
-SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/#{ENV['S3_BUCKET_NAME']}"
+SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/clipreactor"
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
   ENV['S3_BUCKET_NAME'],
   aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
